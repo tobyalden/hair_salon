81b062093d175460cf350ee10b44b8d78b60b61a
Toby <Alden@Tobys-MacBook-Pro.local>
2015-05-08 11:53:31 -0700

    Create Stylist class and CRUD methods. Pass specs.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,3 +1,46 @@
 class Stylist
 
+  attr_reader(:id, :name)
+
+  define_method(:initialize) do |attributes|
+    @id = attributes[:id]
+    @name = attributes[:name]
+  end
+
+  define_singleton_method(:all) do
+    db_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    db_stylists.each() do |db_stylist|
+      id = db_stylist.fetch('id').to_i()
+      name = db_stylist.fetch('name')
+      stylists.push(Stylist.new({:id => id, :name => name}))
+    end
+    stylists
+  end
+
+  define_singleton_method(:find) do |find_id|
+    db_stylist = DB.exec("SELECT * FROM stylists WHERE id = #{find_id}")
+    id = db_stylist.first().fetch('id').to_i()
+    name = db_stylist.first().fetch('name')
+    return Stylist.new({:id => id, :name => name})
+  end
+
+  define_method(:==) do |another_stylist|
+    return @name == another_stylist.name
+  end
+
+  define_method(:save) do
+    db_id = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = db_id.first().fetch('id').to_i()
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes[:name]
+    DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM stylists WHERE id = #{@id}")
+  end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -2,8 +2,65 @@ require('spec_helper')
 
 describe(Stylist) do
 
-  describe(:initialize) do
+  describe('#initialize') do
+    it("Creates a new stylist.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      expect(test_stylist.class).to(eq(Stylist))
+    end
+  end
 
+  describe('#id') do
+    it("Returns the database ID of the stylist.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      expect(test_stylist.id).to(eq(nil))
+    end
+  end
+
+  describe('#name') do
+    it("Returns the name of the stylist.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      expect(test_stylist.name).to(eq("Brenda"))
+    end
+  end
+
+  describe('.all') do
+    it("Returns all the stylists in the database.") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it("Returns the stylist in the database with the given ID") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      test_stylist.save()
+      expect(Stylist.find(test_stylist.id)).to(eq(test_stylist))
+    end
+  end
+
+  describe('#save') do
+    it("Saves the stylist to the database.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe('#update') do
+    it("Updates the stylist's name in the database.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      test_stylist.save()
+      test_stylist.update({:name => "Tina"})
+      expect(Stylist.find(test_stylist.id).name).to(eq("Tina"))
+    end
+  end
+
+  describe('#delete') do
+    it("Deletes the stylist from the database.") do
+      test_stylist = Stylist.new({:id => nil, :name => "Brenda"})
+      test_stylist.save()
+      test_stylist.delete()
+      expect(Stylist.all()).to(eq([]))
+    end
   end
 
 end
